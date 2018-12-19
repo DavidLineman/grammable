f4704a96aee2105789a6d71f2a9987f72114c7b5
David Lineman <dline215@gmail.com>
2018-12-19 14:30:12 +0000

    added test for gram form

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
+
+
 RSpec.describe GramsController, type: :controller do
   describe "grams#index action" do
     it "should successfully show the page" do 
@@ -8,4 +10,10 @@ RSpec.describe GramsController, type: :controller do
     end
   end
 
+  describe "grams#new action" do 
+    it "should successfully show the new form" do 
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
